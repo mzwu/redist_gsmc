de14247dc1c3f8c485b9704298830ea7008f07de
Melissa Wu <mzwu173@gmail.com>
2026-01-02 20:30:40 -0500

    update

--- src/map_calc.h
+++ src/map_calc.h
@@ -427,10 +427,13 @@ double eval_capacity_gsmc_version(
     // Calculate and compare ratio
     double ratio = normalized_pop / pop_capacity;
 
-    if (ratio < 0.85 || ratio > 1.15) {
-        return 2;
+    if (ratio < 0.85) {
+        return 2 + abs(ratio - 0.85);
     }
-    else if ((0.85 <= ratio && ratio <= 0.94) || 1.05 <= ratio <= 1.14) {
+    else if (ratio > 1.14) {
+        return 2 + abs(ratio - 1.14);
+    }
+    else if ((0.85 <= ratio && ratio <= 0.94) || 1.05 <= ratio && ratio <= 1.14) {
         return 1;
     }
     else if (0.95 <= ratio && ratio <= 1.04) {
